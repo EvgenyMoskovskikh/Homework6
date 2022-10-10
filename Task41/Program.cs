304c103d8b5458c7d95543786d38c01fa1a88bf1
evgeny moskovskikh <mosevgwork@yandex.ru>
2022-10-10 19:49:15 +0300

    Нарешал тут как обычно непонятно

---
+++ Task41/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// (int[], int) array = Array(5);
+// (int[], int count) Array(int size)
+// {
+//     int[] result = new int[size];
+//     var count = 0;
+//     Console.Write("Массив [ ");
+//     for (int i = 0; i < size; i++)
+//     {
+//         result[i] = new Random().Next(-1000, 1000);
+//         if (result[i] > 0)
+//         {
+//             count++;
+//         }
+//                 Console.Write(result[i] + " ");
+        
+//     }
+//     Console.Write("] - ");
+//     Console.WriteLine($"Количество чисел больше 0 равно {count}");
+//     return (result, count);
+// }
+
+
+
+Console.Write("Введите числа через пробел: ");
+int[] arr = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Количество чисел больше 0 равно {count}");
+
+
---
+++ Task43/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.Write("Введите значение k1: ");
+int k1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите значение b1: ");
+int b1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите значение k2: ");
+int k2 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите значение b2: ");
+int b2 = int.Parse(Console.ReadLine()!);
+int x = (b2 - b1) / (k1 - k2);
+int y = k1 * x + b1;
+Console.WriteLine($"Точка пересечения двух прямых: ({x};{y})");
